53a67463de4a31bd9f93f529ef3244bf97a98545
danielm96d <danielthommiller@gmail.com>
2024-07-09 10:45:56 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header">My Recipes</h1>
+      <p>There are no recipes to list.</p>
+    </div>
+  );
+}
+
+export default App;
